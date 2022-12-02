57a74d2359435c9ea312d767a8e7d00859b90665
Derek Todd <derekhtodd@yahoo.co.uk>
2022-12-02 15:22:38 +0000

    basic structure added according to the exercise spect

--- assesdEx1.c
+++ assesdEx1.c
@@ -7,10 +7,45 @@ typedef struct node_t
         int value;
         struct node_t* left;
         struct node_t* right;
-    }; 
+    } node_t; 
+
+
+int search(node_t * node, int toFind) {
+
+}
 
 
 
+
+node_t * createTree(int firstElem) {
+    return 1;
+}
+
+
+
+
+void destroyTree(node_t * node) {
+
+}
+
+
+
+
+void insert(node_t * node, int elem) {
+
+}
+
+
+
+void delete(node_t * node, int elem) {
+
+}
+
+
+
+int main() {
+    // mycode here
+}
 
 
 

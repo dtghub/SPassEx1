842c240b222f1f03daf546374dbf395063dfd7a0
Derek Todd <derekhtodd@yahoo.co.uk>
2022-12-05 15:01:35 +0000

    add an outline of what needs done using code comments

--- assesdEx1.c
+++ assesdEx1.c
@@ -12,13 +12,20 @@ typedef struct node_t
 
 int search(node_t * node, int toFind) {
 
+    // search algorithm is; test the node.value to see if it is grater or less than the value you are looking for, if the value you seek is less than node.value, follow left, otherwise follow right.
+    // When found, retunr the node.
+
+
+
+
+
 }
 
 
 
 
 node_t * createTree(int firstElem) {
-    // Use arbtrary size of 100 node for now
+    // create a new root node
     ptr = (node_t*) malloc(100);
     topNode.value = firstElem;
     return ptr;
@@ -29,6 +36,11 @@ node_t * createTree(int firstElem) {
 
 void destroyTree(node_t * node) {
     // free up the menory used for the data struct when done
+
+    // Need to walk through the tree and delete the end (leaf) nodes, making sure we don't orphan any nodes.
+
+    // To find the leaves, walk down the left nodes as far as possible, then come back up until you can go right, and then follow the left, come back up until you can go right etc - each time that you land on a leaf node, delete it and then continue at the parent node...
+
     free(node);
 }
 
@@ -41,6 +53,7 @@ void insert(node_t * node, int elem) {
     // if bigger look at right, if smaller look at left - if the same then don't add
     // if left or right node doesn't exist then add this new node, otherwise repeat previous step
 
+
 }
 
 
@@ -48,7 +61,11 @@ void insert(node_t * node, int elem) {
 void delete(node_t * node, int elem) {
 
 }
+    // search the tree for the element.
 
+    // 1. If the node to be deleted is a leaf, there is nothing else to do.
+    // 2. If the node to be deleted has exactly one child, that child will move up to replace its parent.
+    // 3. If the node to be deleted has two children, you need to find the successor, i.e. the value that would replace the one to be deleted. Typically, this is the smallest value in the right subtree.
 
 
 int main() {
